5fded7308d743c8f0178f4444842379c17444b36
kaurovalexandr <kaurovalexandr@gmail.com>
2019-11-29 06:41:36 +0300

    add logging

--- build.sbt
+++ build.sbt
@@ -5,9 +5,13 @@ version := "0.1"
 scalaVersion := "2.13.1"
 
 val circeVersion = "0.12.3"
+val akkaVersion = "2.6.0"
 
 libraryDependencies ++= Seq(
-  "io.circe" %% "circe-core",
-  "io.circe" %% "circe-generic",
-  "io.circe" %% "circe-parser"
-).map(_ % circeVersion)
+  "io.circe" %% "circe-core" % circeVersion,
+  "io.circe" %% "circe-generic" % circeVersion,
+  "io.circe" %% "circe-parser" % circeVersion,
+
+  "ch.qos.logback" % "logback-classic" % "1.2.3",
+  "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2"
+)
--- src/main/scala/ru/crpt/edi/education/kafka/MessageForSD.scala
+++ src/main/scala/ru/crpt/edi/education/kafka/MessageForSD.scala
@@ -3,19 +3,23 @@ package ru.crpt.edi.education.kafka
 import io.circe.generic.auto._
 import io.circe.parser
 import io.circe.syntax._
-
+import com.typesafe.scalalogging.StrictLogging
 
 case class MessageForSD(ownerId: String, partnerId: String, documentId: String,
-                        filename: String, `type`: Int, status: Byte, comment: String) {
-  def toJson: String = this.asJson.noSpaces
+                        filename: String, `type`: Int, status: Byte, comment: String) extends StrictLogging {
+  def toJson: String = {
+    val json = this.asJson.noSpaces
+    logger.info(s"to json: $json")
+    json
+  }
 }
 
-object MessageForSD {
+object MessageForSD extends StrictLogging {
   def fromJson(json: String): Unit = {
     val decodeResult = parser.decode[MessageForSD](json)
     decodeResult match {
-      case Right(message) => println(message.toString)
-      case Left(error) => println(error.toString)
+      case Right(message) => logger.info(s"from json: $message")
+      case Left(error) => logger.error(s"error : $error")
     }
   }
 }
